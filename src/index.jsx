d38c63d27015325395c1835e16803e0a116d6e7d
LucieSt <strakova.91@gmail.com>
2023-11-02 21:15:51 +0100

    done to 2.6

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,19 @@
 import { createRoot } from 'react-dom/client';
+import { useState } from 'react';
+import Player from './components/Player';
+import Lyrics from './components/Lyrics';
 import './style.css';
+import audioSrc from '/fools-garden-lemon-tree.mp3'
+import lyricsLines from './lyrics-lines';
 
 const App = () => {
+
+  const [activeLine, setActiveLine] = useState(4);
+
   return (
     <div className="container">
-      <h1>Karaoke</h1>
+      <Player audioSrc={audioSrc} />
+      <Lyrics lines={lyricsLines} currentLineIndex={activeLine}/>
     </div>
   );
 };
